32e3d95d76356bb43547ea7b782eb2ed1219435c
Nit8 <47635411+Nit8@users.noreply.github.com>
2024-09-07 00:26:13 +0545

    Initial Code Commit

--- TODOWinApp/Form1.Designer.cs
+++ TODOWinApp/Form1.Designer.cs
@@ -1,14 +1,17 @@
-﻿namespace TODOWinApp
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TODOWinApp
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,18 +26,116 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            textBoxTask = new TextBox();
+            buttonAdd = new Button();
+            listBoxTasks = new ListBox();
+            buttonMarkAs = new Button();
+            buttonDelete = new Button();
+            contextMenuMarkAs = new ContextMenuStrip(components);
+            doneToolStripMenuItem = new ToolStripMenuItem();
+            undoneToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuMarkAs.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBoxTask
+            // 
+            textBoxTask.Location = new Point(12, 12);
+            textBoxTask.Name = "textBoxTask";
+            textBoxTask.Size = new Size(200, 23);
+            textBoxTask.TabIndex = 0;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(218, 11);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(75, 23);
+            buttonAdd.TabIndex = 1;
+            buttonAdd.Text = "Add Task";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // listBoxTasks
+            // 
+            listBoxTasks.FormattingEnabled = true;
+            listBoxTasks.ItemHeight = 15;
+            listBoxTasks.Location = new Point(12, 41);
+            listBoxTasks.Name = "listBoxTasks";
+            listBoxTasks.Size = new Size(280, 154);
+            listBoxTasks.TabIndex = 2;
+            // 
+            // buttonMarkAs
+            // 
+            buttonMarkAs.Location = new Point(12, 201);
+            buttonMarkAs.Name = "buttonMarkAs";
+            buttonMarkAs.Size = new Size(110, 23);
+            buttonMarkAs.TabIndex = 3;
+            buttonMarkAs.Text = "Mark as";
+            buttonMarkAs.UseVisualStyleBackColor = true;
+            buttonMarkAs.Click += buttonMarkAs_Click;
+            // 
+            // buttonDelete
+            // 
+            buttonDelete.Location = new Point(182, 201);
+            buttonDelete.Name = "buttonDelete";
+            buttonDelete.Size = new Size(110, 23);
+            buttonDelete.TabIndex = 4;
+            buttonDelete.Text = "Delete Task";
+            buttonDelete.UseVisualStyleBackColor = true;
+            buttonDelete.Click += buttonDelete_Click;
+            // 
+            // contextMenuMarkAs
+            // 
+            contextMenuMarkAs.Items.AddRange(new ToolStripItem[] { doneToolStripMenuItem, undoneToolStripMenuItem });
+            contextMenuMarkAs.Name = "contextMenuMarkAs";
+            contextMenuMarkAs.Size = new Size(117, 48);
+            // 
+            // doneToolStripMenuItem
+            // 
+            doneToolStripMenuItem.Name = "doneToolStripMenuItem";
+            doneToolStripMenuItem.Size = new Size(116, 22);
+            doneToolStripMenuItem.Text = "Done";
+            doneToolStripMenuItem.Click += doneToolStripMenuItem_Click;
+            // 
+            // undoneToolStripMenuItem
+            // 
+            undoneToolStripMenuItem.Name = "undoneToolStripMenuItem";
+            undoneToolStripMenuItem.Size = new Size(116, 22);
+            undoneToolStripMenuItem.Text = "Undone";
+            undoneToolStripMenuItem.Click += undoneToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(304, 241);
+            Controls.Add(buttonDelete);
+            Controls.Add(buttonMarkAs);
+            Controls.Add(listBoxTasks);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxTask);
+            Name = "Form1";
+            Text = "TODO App";
+            contextMenuMarkAs.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBoxTask;
+        private Button buttonAdd;
+        private ListBox listBoxTasks;
+        private Button buttonMarkAs;
+        private Button buttonDelete;
+        private ContextMenuStrip contextMenuMarkAs;
+        private ToolStripMenuItem doneToolStripMenuItem;
+        private ToolStripMenuItem undoneToolStripMenuItem;
     }
 }
 
--- TODOWinApp/Form1.cs
+++ TODOWinApp/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TODOWinApp
@@ -15,6 +9,68 @@ namespace TODOWinApp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBoxTask.Text))
+            {
+                listBoxTasks.Items.Add(textBoxTask.Text);
+                textBoxTask.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void buttonMarkAs_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                contextMenuMarkAs.Show(buttonMarkAs, new Point(0, buttonMarkAs.Height));
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to mark.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void doneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MarkTask("(Done)");
+        }
+
+        private void undoneToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MarkTask("(Undone)");
+        }
+
+        private void MarkTask(string status)
+        {
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                int index = listBoxTasks.SelectedIndex;
+                string task = listBoxTasks.SelectedItem.ToString();
+
+                // Remove any existing status
+                task = task.Replace("(Done)", "").Replace("(Undone)", "").Trim();
+
+                listBoxTasks.Items.RemoveAt(index);
+                listBoxTasks.Items.Insert(index, $"{task} {status}");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxTasks.SelectedIndex != -1)
+            {
+                listBoxTasks.Items.RemoveAt(listBoxTasks.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
